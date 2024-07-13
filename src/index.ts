de631426654ef515cbb3434cf43167d414f022fa
ecc97 <carmonaedwin1123@gmail.com>
2024-07-13 12:48:03 -0500

    feat: create index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!!")
+import { IResponse } from './interface/interface.js'
+import { ListTemplate } from './classes/listTemplate.js';
+
+const div  = document.querySelector('.item-list') as HTMLUListElement;
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const data: IResponse[] = await getAllUsers();
+    const list = new ListTemplate(div);
+
+    data.map((user: IResponse) => {
+        list.render(user);
+    })
+})
+
+async function getAllUsers(): Promise<IResponse[]> {
+    const response = await fetch("http://localhost:3000/users")
+    const data = await response.json()
+    return data  
+}
